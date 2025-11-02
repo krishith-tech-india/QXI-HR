cd1aba3520357b4a6780d545a3920d72e9635d7a
krishith-tech-india <hr@krishith.com>
2025-11-02 08:31:54 +0530

    Triggring work flow

--- API/DIExtensions.cs
+++ API/DIExtensions.cs
@@ -40,8 +40,6 @@ namespace API
                 });
 
             });
-
-            
         }
 
         public static void AddJWT(this IServiceCollection services, IConfiguration configuration)
